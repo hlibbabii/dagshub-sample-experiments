054208aef914f706123a61c9a86ffc9628846c8d
hlib <hlibbabii@gmail.com>
2020-05-24 12:28:02 +0200

    [NOT experiment] rename the names of parameters

--- calc-cylinder-volume.py
+++ calc-cylinder-volume.py
@@ -14,7 +14,7 @@ input_file, output_file = sys.argv[1:]
 with open(input_file) as f:
     param_dict = json.load(f)
 
-volume = calc_volume(param_dict['r'], param_dict['h'])
+volume = calc_volume(param_dict['radius'], param_dict['height'])
 
 with dagshub_logger(metrics_path=output_file, should_log_hparams=False) as logger:
     logger.log_metrics({'volume': volume}, step_num=1)
